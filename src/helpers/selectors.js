6619adf1e91814cfd1e710421df0253effde5ba1
Najib <najib.muqeem@gmail.com>
2020-03-18 19:22:39 -0400

    transforming data with selectors

---
+++ src/helpers/selectors.js
@@ -0,0 +1,18 @@
+export function getAppointmentsForDay(state, day) {
+  let filteredDays = state.days.filter(newDay => day === newDay.name);
+
+  if (!(filteredDays !== [] && day && filteredDays[0])) {
+    return [];
+  }
+
+  const appointments = filteredDays[0].appointments;
+
+  const result = [];
+
+  for (let key in state.appointments) {
+    if (appointments.includes(state.appointments[key].id)) {
+      result.push(state.appointments[key]);
+    }
+  }
+  return result;
+}
