eb2806bc2cd1a2331f40d36fc7d4e3970ee13878
Najib <najib.muqeem@gmail.com>
2020-03-23 16:06:07 -0400

    fix import of useVisualMode

--- src/hooks/__tests__/useVisualMode.test.js
+++ src/hooks/__tests__/useVisualMode.test.js
@@ -1,6 +1,6 @@
 import { renderHook, act } from "@testing-library/react-hooks";
 
-import useVisualMode from "hooks/useVisualMode";
+import { useVisualMode } from "hooks/useVisualMode";
 
 const FIRST = "FIRST";
 
