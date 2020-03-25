50ca510c875c4b24bd8442d82205b3a84fecf9a7
Najib <najib.muqeem@gmail.com>
2020-03-25 14:30:51 -0400

    the final tests, fix for selectors

---
+++ cypress/integration/appointments.spec.js
@@ -0,0 +1,46 @@
+describe("Appointments", () => {
+  beforeEach(() => {
+    cy.request("GET", "/api/debug/reset");
+
+    cy.visit("/");
+
+    cy.contains("Monday");
+  });
+
+  it("should book an interview", () => {
+    cy.get("[alt=Add]")
+      .first()
+      .click();
+
+    cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
+
+    cy.contains("Save").click();
+
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+  });
+
+  it("should edit an interview", () => {
+    cy.get("[alt=Edit]")
+      .first()
+      .click({ force: true });
+
+    cy.get("[data-testid=student-name-input]")
+      .clear()
+      .type("Lydia Miller-Jones");
+
+    cy.contains("Save").click();
+
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+  });
+
+  it("should cancel an interview", () => {
+    cy.get("[alt=Delete]").click({ force: true });
+
+    cy.contains("Confirm").click();
+
+    cy.contains("Deleting").should("exist");
+    cy.contains("Deleting").should("not.exist");
+
+    cy.contains(".appointment__card--show", "Archie Cohen").should("not.exist");
+  });
+});
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,59 +1,31 @@
 export function getAppointmentsForDay(state, day) {
-  let filteredDays = state.days.filter(newDay => day === newDay.name);
-
-  if (!(filteredDays !== [] && day && filteredDays[0])) {
+  const foundDay = state.days.find(stateDay => day === stateDay.name);
+  if (!foundDay) {
     return [];
   }
-
-  const appointments = filteredDays[0].appointments;
-
-  const result = [];
-
-  for (let key in state.appointments) {
-    if (appointments.includes(state.appointments[key].id)) {
-      result.push(state.appointments[key]);
-    }
-  }
-  return result;
+  const foundAppointments = foundDay.appointments.map(appointment => {
+    return state.appointments[appointment];
+  });
+  return foundAppointments;
 }
 
 export function getInterviewersForDay(state, day) {
-  let filteredDays = state.days.filter(stateDay => day === stateDay.name);
-
-  if (!(filteredDays !== [] && day && filteredDays[0])) {
+  const foundDay = state.days.find(stateDay => day === stateDay.name);
+  if (!foundDay || foundDay.interviewers.length === 0) {
     return [];
   }
-
-  const { appointments } = filteredDays[0];
-
-  const interviewers = [];
-  for (let appt of Object.values(state.appointments)) {
-    if (!appointments.includes(appt.id) && appt.interview) {
-      let interviewer = appt.interview.interviewer.toString();
-      if (!interviewers.includes(state.interviewers[interviewer])) {
-        interviewers.push(state.interviewers[interviewer]);
-      }
-    }
-  }
-
-  return interviewers;
+  const foundInterviewers = foundDay.interviewers.map(interviewer => {
+    return state.interviewers[interviewer];
+  });
+  return foundInterviewers;
 }
 
 export function getInterview(state, interview) {
   if (!interview) {
     return null;
   }
-  const { interviewer } = interview;
-  let name = "";
-  let avatar = "";
-  for (const key in state.interviewers) {
-    if (state.interviewers[key].id === interviewer) {
-      name = state.interviewers[key].name;
-      avatar = state.interviewers[key].avatar;
-    }
-  }
   return {
     student: interview.student,
-    interviewer: { id: interviewer, name, avatar }
+    interviewer: state.interviewers[interview.interviewer]
   };
 }
